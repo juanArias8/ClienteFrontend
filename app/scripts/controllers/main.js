e67776467ac990b604defe9fca50e0f9e7735ae3
juasda96 <juasda96@gmail.com>
2017-05-30 00:57:43 -0500

    Commit May 30

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,20 @@
 'use strict';
-
-/**
- * @ngdoc function
- * @name clienteFrontendApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the clienteFrontendApp
- */
 angular.module('clienteFrontendApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        .controller('MainCtrl', function ($scope) {
+            $scope.integrantes = [
+                {name: 'Juan David Arias'},
+                {name: 'Juan Diego Góez'},
+                {name: 'Alejandro Castañeda'},
+                {name: 'Jesús David Gómez'}
+            ];
+            $scope.mensaje = {
+                titulo: 'LABORATORIO NÚMERO 5',
+                area: 'Arquitectura de software',
+                universidad: 'Universidad de Antioquia',
+                profesor: 'Diego José Luis Botia ',
+                descripcion: 'Laboratorio número cinco de la meteria Arquitectura de software.......\n\
+                              Se agregan estilos utilizando el Framework Bootstrap en su versión 3.3.7......\n\
+                              La lógica del frontend se desarrolla utilizando el framework AngularJs......\n\
+                              Los datos se envían y reciben utilizando rest soportado en JSON......'
+            };
+        });
