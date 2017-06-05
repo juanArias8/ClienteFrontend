6a0509b737c246d21d097bebce208084fe4432a4
juasda96 <juasda96@gmail.com>
2017-06-05 12:01:25 -0500

    Final commit

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,11 +1,13 @@
-angular.module('customerApp.services',[]).factory('Customer',function($resource){
-    return $resource('http://localhost:8080/ClienteBackend/webresources/com.edu.udea.clientebackend.entity.cliente/:id',{id:'@id'},{
+angular.module('customerApp.services', []).factory('Customer', function ($resource) {
+    return $resource(
+            'http://localhost:8080/ClienteBackend/webresources/com.edu.udea.clientebackend.entity.cliente/:id'
+            , {id: '@id'}, {
         update: {
             method: 'PUT'
         }
     });
-}).service('popupService',function($window){
-    this.showPopup=function(message){
+}).service('popupService', function ($window) {
+    this.showPopup = function (message) {
         return $window.confirm(message);
     };
 });
